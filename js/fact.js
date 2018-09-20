30d8bdab4ee3729fa42cb621a090c883a16446dd
konstantin0s <constantintofan85@gmail>
2018-09-20 19:39:24 +0200

    Add final javascript function

--- js/fact.js
+++ js/fact.js
@@ -2,20 +2,35 @@ let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 
 let numberInput = document.querySelector('#numberInput');
-numberInput.addEventListener('input', getFactAjax);
+numberInput.addEventListener('input', getFactFetch);
 
-function getFactAjax() {
+// function getFactAjax() {
+//   let number = numberInput.value;
+//
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'http://numbersapi.com/'+number);
+//
+//   xhr.onload = function() {
+//     if (this.status == 200 && number != '') {
+//       fact.style.display = 'block';
+//       factText.innerText = this.responseText;
+//     }
+//   }
+//
+//   xhr.send();
+// }
+
+
+function getFactFetch() {
   let number = numberInput.value;
 
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://numbersapi.com/'+number);
-
-  xhr.onload = function() {
-    if (this.status == 200 && number != '') {
-      fact.style.display = 'block';
-      factText.innerText = this.responseText;
-    }
+  fetch('http://numbersapi.com/'+number)
+  .then(response => response.text())
+  .then(data => {
+  if (number != '') {
+    fact.style.display = 'block';
+    factText.innerText = data;
   }
-
-  xhr.send();
+  })
+  .catch(err => console.log(err));
 }
