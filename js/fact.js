bad36da3b1cebdc85fefe92e45c13c8daca22c59
konstantin0s <constantintofan85@gmail>
2018-09-20 19:22:05 +0200

    Add javascript file(setup variables)

---
+++ js/fact.js
@@ -0,0 +1,9 @@
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+
+let numberInput = document.querySelector('#numberInput');
+numberInput.addEventListener('input', getFactAjax);
+
+function getFactAjax() {
+  console.log(123);
+}
